a675786dbf718fa03f94dc8b85d3594cefe9c800
sickunicorn24 <aubreytull@ufl.edu>
2024-11-13 17:21:28 -0500

    update

--- whackamole.py
+++ whackamole.py
@@ -1,58 +1,60 @@
 import pygame
 import random
 #Added comment
+def draw_grid(screen, line_color, square_size, width, height):
+    #draw horizontal lines
+    for i in range(1, 16):
+        pygame.draw.line(
+            screen,
+            line_color,
+            (0, i*square_size),
+            (width, i*square_size),
+
+        )
+
+    #draw vertical lines
+    for i in range(1, 20):
+        pygame.draw.line(
+            screen,
+            line_color,
+            (i*square_size, 0),
+            (i*square_size, height),
+
+        )
 
 def main():
-    def draw_grid():
-        #draw horizontal lines
-        for i in range(1, 16):
-            pygame.draw.line(
-                screen, 
-                line_color, 
-                (0, i), 
-                (width, i), 
-                line_width
-            )
 
-        #draw vertical lines
-        for i in range(1, 20):
-            pygame.draw_line(
-                screen, 
-                line_color, 
-                (i, 0),
-                (i, height), 
-                line_width
-            )
-    
-    def draw_mole():
-        rand_x = random.randint(1, 640)
-        rand_y = random.randint(1, 512)
-        screen.blit(mole_image, mole_image.get_rect(topleft = (0,0)))
-        
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = event.pos
-            screen.blit(mole_image, mole_image.get_rect(center = (rand_x, rand_y)))
-                    
     try:
         pygame.init()
         width = 640
         height = 512
-        line_width = 5
         line_color = "black"
-        
-        # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
+        square_size = 32
+
+
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((width, height))
         clock = pygame.time.Clock()
         running = True
-            
+
         while running:
+            rand_x = random.randint(1, 21)
+            rand_y = random.randint(1, 17)
+            mole_rect = mole_image.get_rect(topleft = (rand_x, rand_y))
+
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                    
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = event.pos
+
+                    if event.pos == mole_rect:
+                        screen.blit(mole_image, mole_rect)
+
             screen.fill("light pink")
+            draw_grid(screen, line_color, square_size, width, height)
+            screen.blit(mole_image, mole_image.get_rect(topleft = (0,0)))
             pygame.display.flip()
             clock.tick(60)
          
