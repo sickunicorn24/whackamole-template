df64ecec5a120c323831a34b4b47ee327ec0bd1c
sickunicorn24 <aubreytull@ufl.edu>
2024-11-13 20:21:52 -0500

    update

--- whackamole.py
+++ whackamole.py
@@ -39,33 +39,47 @@ def main():
         clock = pygame.time.Clock()
         running = True
 
+        current_x = 0
+        current_y = 0
+
+        screen.fill("black")
+        draw_grid(screen, line_color, square_size, width, height)
+        screen.blit(mole_image, mole_image.get_rect(topleft = (current_x, current_y)))
+        pygame.display.flip()
+        clock.tick(60)
+
+        rand_x = random.randint(0, columns)
+        rand_y = random.randint(0, rows)
+
         while running:
-            current_x = 0
-            current_y = 0
-            screen.fill("black")
-            draw_grid(screen, line_color, square_size, width, height)
-            screen.blit(mole_image, mole_image.get_rect(topleft = (current_x, current_y)))
-            pygame.display.flip()
-            clock.tick(60)
-
-            rand_x = random.randint(0, columns)
-            rand_y = random.randint(0, rows)
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
+
+                elif event.type == pygame.MOUSEBUTTONDOWN:
                     x, y = event.pos
 
-                    if (current_x * 32 < x < current_x+1 * 32) and (current_y * 32 < y < current_y+1 * 32):
-                        current_x = rand_x * 32
-                        current_y = rand_y * 32
+                    start_x = current_x * square_size
+                    end_x = (current_x+1) * square_size
 
-                        mole_rect = mole_image.get_rect(topleft = (current_x, current_y))
+                    start_y = current_y * square_size
+                    end_y = (current_y+1) * square_size
 
-                        screen.blit(mole_image, mole_rect)
+                    if (start_x <= x <= end_x) and (start_y <= y <= end_y):
+                        current_x = rand_x * square_size
+                        current_y = rand_y * square_size
+
+                    screen.fill("black")
+                    draw_grid(screen, line_color, square_size, width, height)
+
+                    screen.blit(mole_image, mole_image.get_rect(topleft = (current_x, current_y)))
+                    pygame.display.flip()
+
+                    print(current_x)
+                    print(current_y)
     finally:
         pygame.quit()
+
 
 if __name__ == "__main__":
     main()
